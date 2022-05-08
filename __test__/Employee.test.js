0a2a65b4db2cd8081e41cc9469d22d560d857c20
shiba inu <jefferson121996@gmail.com>
2022-05-08 11:11:23 -0400

    changes made

---
+++ __test__/Employee.test.js
@@ -0,0 +1,25 @@
+// const Employee = require('../lib/Employee')
+
+// test('creates an employee object', () => {
+//     const employee = new Employee(name);
+//     const name = "Marcus";
+//     expect(employee.name).toBe(name);
+// });
+
+// test('setting an employee id', () => {
+//     const id = "1";
+//     const employee = new Employee(id);
+//     expect(employee.id).toBe(id);
+// });
+
+// test('setting up employee email with getEmail()', () => {
+//     const email = "Marcus@macus.com";
+//     const employee = new Employee(email);
+//     expect(employee.getEmail()).toBe(email);
+// });
+
+// test(" Running getRole() should return 'Employee'", () => {
+//     const role = "Employee"
+//     const employee = new Employee(role);
+//     expect(employee.getRole()).toBe(role);
+// });
